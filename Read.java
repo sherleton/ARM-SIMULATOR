dfd75dbecefa2266d88c11460700fd76e6cc9eed
Nikhil Sachdeva <nikhil16061@iiitd.ac.in>
2017-11-28 05:57:03 +0000

    SECOND

--- Read.java
+++ Read.java
@@ -1,6 +1,30 @@
 import java.util.*;
 import java.io.*;
 
+class Instruction
+{
+	String _31_28_;
+	String dp;
+	String immediate;
+	String opcode;
+	String s;
+	String op1;
+	String dest;
+	String op2;
+
+	Instruction(String bin)
+	{
+		_31_28_ = bin.substring(0,4);
+		dp = bin.substring(4,6);
+		immediate = bin.substring(6,7);
+		opcode = bin.substring(7,11);
+		s = bin.substring(11,12);
+		op1 = bin.substring(12,16);
+		dest = bin.substring(16,20);
+		op2 = bin.substring(20,32);
+	}
+}
+
 public class Read
 {
 	public static ArrayList<String> read(String file) throws IOException
@@ -36,7 +60,16 @@ public class Read
 			instructions.set(i, Long.toBinaryString(Long.parseLong(instructions.get(i).split(" ")[1].substring(2), 16)));
 		}
 
+		ArrayList<Instruction> coded = new ArrayList<Instruction>();
 		for(int i = 0; i < instructions.size(); i++)
-			System.out.println(addresses.get(i) + " " + instructions.get(i));
+			coded.add(new Instruction(instructions.get(i)));
+
+		Instruction st = null;
+		for(int i = 0; i < coded.size(); i++)
+		{
+			st = coded.get(i);
+			System.out.println(addresses.get(i) + "   " + st._31_28_ + " " + st.dp + " " + st.immediate + " " + st.opcode + " " + st.s + " " + st.op1 + " " + st.dest + " " + st.op1);
+		}
+
 	}
-} 
+}
